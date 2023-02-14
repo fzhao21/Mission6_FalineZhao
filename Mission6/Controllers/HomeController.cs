98546bfbfe0f71eb62c184c53d62567a8015b112
Yunxi Zhao <falinezhao21@gmail.com>
2023-02-13 22:00:03 -0700

    Add project files.

---
+++ Mission6/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission6.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission6.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private MovieAdditionContext blahContext { get; set; }
+        public HomeController(ILogger<HomeController> logger, MovieAdditionContext someName)
+        {
+            _logger = logger;
+            blahContext = someName;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult PodCast()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult MovieForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult MovieForm(ApplicationResponse ar)
+        {
+            if (ModelState.IsValid)
+            {
+                blahContext.Add(ar);
+                blahContext.SaveChanges();
+                return View("Confirmatiion");
+            }
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
